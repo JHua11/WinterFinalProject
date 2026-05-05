e50fdabd118846f58437673f94f8d24cb5d4f017
Jacob Huang <jacob.huang.11@gmail.com>
2026-05-05 17:32:17 -0400

    Add methods for reportable

--- src/main/java/interfaces/Reportable.java
+++ src/main/java/interfaces/Reportable.java
@@ -1,4 +1,15 @@
 package interfaces;
 
 public interface Reportable {
+    /**
+     * Creates a string including details for every user in the library
+     * @return the created string
+     */
+    public static String genItemReport() {}
+
+    /**
+     * Creates a string including details for every item in the library
+     * @return the created string
+     */
+    public static String genUserReport() {}
 }
