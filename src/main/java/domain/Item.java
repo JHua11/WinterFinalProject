882b4ce848dd21128b573fb911f5d1b33d183cf8
Jacob Huang <jacob.huang.11@gmail.com>
2026-05-05 16:55:50 -0400

    Add fields and methods for item class and subclasses

--- src/main/java/domain/Item.java
+++ src/main/java/domain/Item.java
@@ -1,8 +1,29 @@
 package domain;
 
+import lombok.*;
+
+@AllArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
+@ToString
 public abstract class Item {
+    protected String id;
+    protected String title;
+    protected Status status;
+
+    protected static int nextId = 1;
+
+    public Item(String title) {
+        this.id = String.format("%05d", nextId++);
+        this.title = title;
+        this.status = Status.IN_STORE;
+        // TODO: add to item list in library
+    }
 
     public enum Status {
-
+        BORROWED,
+        IN_STORE,
+        LOST
     }
 }
