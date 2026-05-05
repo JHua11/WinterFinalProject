389bea83fc29a124f564d73c8d993843b51a8500
Jacob Huang <jacob.huang.11@gmail.com>
2026-05-05 16:53:26 -0400

    Add max number of book/item constant

--- src/main/java/domain/Student.java
+++ src/main/java/domain/Student.java
@@ -6,6 +6,7 @@ import lombok.ToString;
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 public class Student extends User {
+    private static final int MAX_BOOKS = 5;
     /**
      * Allows students to borrow a book, up to 5 total
      * @param book the book to be borrowed
--- src/main/java/domain/Teacher.java
+++ src/main/java/domain/Teacher.java
@@ -6,6 +6,7 @@ import lombok.ToString;
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 public class Teacher extends User {
+    private static final int MAX_ITEMS = 10;
 
     /**
      * Allows teachers to borrow an item, up to 10 total
