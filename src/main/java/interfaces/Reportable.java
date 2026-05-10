75b388afba790177906b0e14c4e6da5dfba77928
Jacob Huang <jacob.huang.11@gmail.com>
2026-05-10 18:03:43 -0400

    Implement Reportable methods

--- src/main/java/interfaces/Reportable.java
+++ src/main/java/interfaces/Reportable.java
@@ -1,15 +1,31 @@
 package interfaces;
 
-public interface Reportable {
-    /**
-     * Creates a string including details for every user in the library
-     * @return the created string
-     */
-    static String genItemReport() {}
+import domain.Item;
+import domain.Library;
+import domain.User;
 
+public interface Reportable {
     /**
      * Creates a string including details for every item in the library
      * @return the created string
      */
-    static String genUserReport() {}
+    static String genItemReport() {
+        StringBuilder report = new StringBuilder();
+        for (Item item : Library.items) {
+            report.append(item).append("\n");
+        }
+        return report.toString();
+    }
+
+    /**
+     * Creates a string including details for every user in the library
+     * @return the created string
+     */
+    static String genUserReport() {
+        StringBuilder report = new StringBuilder();
+        for (User user : Library.users) {
+            report.append(user).append("\n");
+        }
+        return report.toString();
+    }
 }
